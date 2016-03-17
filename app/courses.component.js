95e34846c40856c70ae88f7e9dd0292840fba730
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2016-03-18 00:45:25 +0600

    Creating Components & Experimenting with Dependency Injection

--- app/courses.component.js
+++ app/courses.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './courses.component', './authors.component'], function(exports_1, context_1) {
+System.register(['angular2/core', './course.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,35 +10,34 @@ System.register(['angular2/core', './courses.component', './authors.component'],
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, courses_component_1, authors_component_1;
-    var AppComponent;
+    var core_1, course_service_1;
+    var CoursesComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (courses_component_1_1) {
-                courses_component_1 = courses_component_1_1;
-            },
-            function (authors_component_1_1) {
-                authors_component_1 = authors_component_1_1;
+            function (course_service_1_1) {
+                course_service_1 = course_service_1_1;
             }],
         execute: function() {
-            AppComponent = (function () {
-                function AppComponent() {
+            CoursesComponent = (function () {
+                function CoursesComponent(courseService) {
+                    this.title = "The title of courses page";
+                    this.courses = courseService.getCourses();
                 }
-                AppComponent = __decorate([
+                CoursesComponent = __decorate([
                     core_1.Component({
-                        selector: 'my-app',
-                        template: "\n        <h1>My First Angular 2 App</h1>\n        <courses></courses>\n        <authors></authors>\n        ",
-                        directives: [courses_component_1.CoursesComponent, authors_component_1.AuthorsComponent]
+                        selector: 'courses',
+                        template: "\n        <h2>Courses</h2>\n        {{title}}\n        <ul>\n            <li *ngFor=\"#course of courses\">{{course}}</li>\n        </ul>\n    ",
+                        providers: [course_service_1.CourseService]
                     }), 
-                    __metadata('design:paramtypes', [])
-                ], AppComponent);
-                return AppComponent;
+                    __metadata('design:paramtypes', [course_service_1.CourseService])
+                ], CoursesComponent);
+                return CoursesComponent;
             }());
-            exports_1("AppComponent", AppComponent);
+            exports_1("CoursesComponent", CoursesComponent);
         }
     }
 });
-//# sourceMappingURL=app.component.js.map
+//# sourceMappingURL=courses.component.js.map
