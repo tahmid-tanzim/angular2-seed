62c5484d0dc41f05c094a14bd117b9cac9d21243
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2016-05-23 21:55:42 +0600

    Creating a subcomponent

--- builds/development/js/app.component.js
+++ builds/development/js/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './artist-item.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,12 +10,15 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, artist_item_component_1;
     var AppComponent, ARTISTS;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (artist_item_component_1_1) {
+                artist_item_component_1 = artist_item_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
@@ -26,6 +29,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                     core_1.Component({
                         selector: 'my-app',
                         templateUrl: 'partials/app.html',
+                        directives: [artist_item_component_1.ArtistItemComponent],
                         styleUrls: ['css/app.css']
                     }), 
                     __metadata('design:paramtypes', [])
---
+++ process/typescript/artist-item.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'artist-item',
+    templateUrl: 'partials/artist-item.html',
+    inputs: ['artist']
+})
+
+export class ArtistItemComponent {
+
+}
